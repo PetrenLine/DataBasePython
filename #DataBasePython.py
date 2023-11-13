1c134d1d9787b51b0000b4d7770bdc53feda5ffd
PetrLine <aweryn852@mail.ru>
2023-11-14 00:03:34 +0300

    Fix bags

--- #DataBasePython.py
+++ #DataBasePython.py
@@ -6,23 +6,20 @@ import sqlite3
 from os.path import join
 
 def register():
-    # Получаем значения из полей ввода
     gender = gender_combobox.get()
     full_name = name_entry.get()
     phone = phone_entry.get()
     password = password_entry.get()
     birth_date = dob_entry.get_date()
 
-    # Сохраняем данные в базе данных
     save_to_database(gender, full_name, phone, password, birth_date)
 
 def save_to_database(gender, full_name, phone, password, birth_date):
-    # Подключаемся к базе данных (если файла нет, SQLite создаст новый)
     db_path = join(".", "registration_data.db")
     conn = sqlite3.connect(db_path)
     cursor = conn.cursor()
 
-    # Создаем таблицу, если она не существует
+
     cursor.execute('''
         CREATE TABLE IF NOT EXISTS users (
             id INTEGER PRIMARY KEY AUTOINCREMENT,
@@ -34,28 +31,23 @@ def save_to_database(gender, full_name, phone, password, birth_date):
         )
     ''')
 
-    # Вставляем данные в таблицу
     cursor.execute('''
         INSERT INTO users (gender, full_name, phone, password, birth_date)
         VALUES (?, ?, ?, ?, ?)
-    ''', (gender, full_name, phone, password, str(birth_date)))  # Преобразуем дату в строку
+    ''', (gender, full_name, phone, password, str(birth_date)))  
 
-    # Сохраняем изменения и закрываем соединение
     conn.commit()
     conn.close()
 
-    # Оповещаем пользователя об успешной регистрации
     messagebox.showinfo("Регистрация", "Регистрация успешно завершена!")
 
 def show_table():
-    # Открываем новое окно для отображения таблицы
+
     table_window = tk.Toplevel(window)
     table_window.title("Таблица пользователей")
 
-    # Создаем Treeview для отображения таблицы
     tree = ttk.Treeview(table_window, columns=('ID', 'Пол', 'ФИО', 'Телефон', 'Пароль', 'Дата рождения'), show='headings')
 
-    # Устанавливаем заголовки столбцов
     tree.heading('ID', text='ID')
     tree.heading('Пол', text='Пол')
     tree.heading('ФИО', text='ФИО')
@@ -63,7 +55,7 @@ def show_table():
     tree.heading('Пароль', text='Пароль')
     tree.heading('Дата рождения', text='Дата рождения')
 
-    # Выбираем данные из базы данных и отображаем их в Treeview
+
     conn = sqlite3.connect(join(".", "registration_data.db"))
     cursor = conn.cursor()
     cursor.execute("SELECT * FROM users")
@@ -71,17 +63,14 @@ def show_table():
     for row in rows:
         tree.insert("", tk.END, values=row)
 
-    # Устанавливаем Treeview на окно
     tree.pack()
 
-    # Добавляем кнопки для редактирования и удаления записей
     edit_button = tk.Button(table_window, text="Редактировать", command=lambda: edit_record(tree))
     edit_button.pack()
 
     delete_button = tk.Button(table_window, text="Удалить", command=lambda: delete_record(tree))
     delete_button.pack()
 
-    # Добавляем Entry и кнопку для поиска
     search_entry = tk.Entry(table_window)
     search_entry.pack()
 
@@ -90,20 +79,15 @@ def show_table():
 
 
 def edit_record(tree):
-    # Получаем выделенную запись
     selected_item = tree.selection()
     if not selected_item:
         messagebox.showwarning("Предупреждение", "Выберите запись для редактирования.")
         return
-
-    # Получаем данные из выделенной записи
     record_id, gender, full_name, phone, password, birth_date = tree.item(selected_item)['values']
 
-    # Создаем новое окно для редактирования
     edit_window = tk.Toplevel(window)
     edit_window.title("Редактирование записи")
 
-    # Добавляем элементы управления для редактирования
     gender_label = tk.Label(edit_window, text="Пол:")
     gender_label.grid(row=0, column=0)
     gender_entry = tk.Entry(edit_window)
@@ -135,7 +119,6 @@ def edit_record(tree):
     dob_entry.grid(row=4, column=1)
     dob_entry.set_date(birth_date)
     
-    # Функция для сохранения изменений
     def save_changes():
         updated_gender = gender_entry.get()
         updated_full_name = name_entry.get()
@@ -143,85 +126,68 @@ def edit_record(tree):
         updated_password = password_entry.get()
         updated_birth_date = dob_entry.get_date()
 
-        # Обновляем запись в базе данных
         update_record(record_id, updated_gender, updated_full_name, updated_phone, updated_password, updated_birth_date)
 
-        # Обновляем отображение в Treeview
         update_record_in_table(tree, selected_item, record_id, updated_gender, updated_full_name, updated_phone, updated_password, updated_birth_date)
 
-        # Закрываем окно редактирования
         edit_window.destroy()
 
 
-# Кнопка для сохранения изменений
     save_button = tk.Button(edit_window, text="Сохранить изменения", command=save_changes)
     save_button.grid(row=5, column=0, columnspan=2)
 
 
 def update_record_in_table(tree, item, record_id, gender, full_name, phone, password, birth_date):
-    # Обновляем отображение в Treeview
     tree.item(item, values=(record_id, gender, full_name, phone, password, birth_date))
 
 
 def update_record(record_id, gender, full_name, phone, password, birth_date):
-    # Подключаемся к базе данных
     conn = sqlite3.connect(join(".", "registration_data.db"))
     cursor = conn.cursor()
 
-    # Обновляем запись в таблице
     cursor.execute('''
         UPDATE users
         SET gender=?, full_name=?, phone=?, password=?, birth_date=?
         WHERE id=?
     ''', (gender, full_name, phone, password, str(birth_date), record_id))
 
-    # Сохраняем изменения и закрываем соединение
     conn.commit()
     conn.close()
 
     messagebox.showinfo("Обновление", "Запись успешно обновлена.")
 
 def delete_record(tree):
-    # Получаем выделенную запись
     selected_item = tree.selection()
     if not selected_item:
         messagebox.showwarning("Предупреждение", "Выберите запись для удаления.")
         return
 
-    # Получаем ID записи
     record_id = tree.item(selected_item)['values'][0]
 
-    # Удаляем запись из базы данных
     conn = sqlite3.connect(join(".", "registration_data.db"))
     cursor = conn.cursor()
     cursor.execute('DELETE FROM users WHERE id=?', (record_id,))
     conn.commit()
     conn.close()
 
-    # Обновляем отображение таблицы
     show_table()
 
     messagebox.showinfo("Удаление", "Запись успешно удалена.")
 
 def search_records(tree, search_text):
-    # Выбираем данные из базы данных, соответствующие поисковому запросу
     conn = sqlite3.connect(join(".", "registration_data.db"))
     cursor = conn.cursor()
     cursor.execute("SELECT * FROM users WHERE full_name LIKE ?", ('%' + search_text + '%',))
     rows = cursor.fetchall()
 
-    # Очищаем Treeview
     tree.delete(*tree.get_children())
 
-    # Вставляем результаты поиска в Treeview
     for row in rows:
         tree.insert("", tk.END, values=row)
 
-# Создаем главное окно
 window = tk.Tk()
 window.title("Регистрация")
 
-# Создаем элементы управления на главном окне
 gender_label = tk.Label(window, text="Пол:")
 gender_label.pack()
 gender_combobox = ttk.Combobox(window, values=["Мужской", "Женский"])
@@ -252,10 +218,8 @@ dob_entry.pack()
 register_button = tk.Button(window, text="Зарегистрироваться", command=register)
 register_button.pack()
 
-# Кнопка для отображения таблицы
 show_table_button = tk.Button(window, text="Отобразить таблицу", command=show_table)
 show_table_button.pack()
 
 
-# Запускаем главный цикл событий Tkinter
 window.mainloop()
